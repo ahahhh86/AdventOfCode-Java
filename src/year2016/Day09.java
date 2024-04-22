a1237b48bab8e4563d957dbf9473844cc99ed87a
ahahhh86 <92790113+ahahhh86@users.noreply.github.com>
2024-04-22 13:56:34 +0200

    new

--- src/year2016/Day09.java
+++ src/year2016/Day09.java
@@ -42,8 +42,44 @@ public class Day09 extends Day00 {
 			}
 		}
 
+
+		// TODO: part 2
+		private static long getDecompressedLength(String str) {
+			if (!str.contains("x")) { return str.length(); }
+
+			var buffer = str.split("[\\(x\\)]", 4);
+
+			int repeatedLength = Integer.parseInt(buffer[1]);
+			int timesRepeated = Integer.parseInt(buffer[2]);
+			var x1 = buffer[0].length();
+			var x2 = getDecompressedLength(buffer[3]);
+
+			var decomprLength = x1 + x2 * timesRepeated;
+
+			return decomprLength;
+		}
+
+		private CompressionData decompressPart2(CompressionData d) { // TODO remove unnecessary operations
+			var start = d.start;
+			var size = d.size;
+
+			int posOpen = source.substring(start).indexOf('(');
+			if (posOpen < 0) { return new CompressionData(-1, size + source.length() - start); }
+
+			posOpen += start; // set to actual position
+			size += posOpen - start;
+			++posOpen; // continue after '('
+			int posClose = source.substring(posOpen).indexOf(')') + posOpen;
+			var marker = source.substring(posOpen, posClose).split("x");
+			int repeatedLength = Integer.parseInt(marker[0]);
+			int timesRepeated = Integer.parseInt(marker[1]);
+			size += getDecompressedLength(source.substring(posClose + 1, posClose + repeatedLength + 1)) * timesRepeated;
+
+			return new CompressionData(posClose + repeatedLength + 1, size);
+		}
+
 		public long decompressPart2() {
-			for (var i = decompressPart1(new CompressionData(0, 0));; i = decompressPart1(i)) {
+			for (var i = decompressPart2(new CompressionData(0, 0));; i = decompressPart1(i)) {
 				if (i.start < 0) { return i.size; }
 			}
 		}
@@ -77,6 +113,7 @@ public class Day09 extends Day00 {
 
 
 
+		System.out.println("\n\n");
 		file = new CompressedFile("(3x3)XYZ");
 		io.printTest(file.decompressPart2(), 9L);
 
